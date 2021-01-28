aabe842d4bd1d3134a0096c8056c13f8200adca8
vuvko <notvuvko@gmail.com>
2021-01-29 00:42:17 +0300

    messy model training pipeline

--- src/craft_ml/pipeline/builder.py
+++ src/craft_ml/pipeline/builder.py
@@ -8,7 +8,7 @@ class Pipeline:
     def __init__(self, serialized_pipeline: str):
         self.block_params: t.List[BlockParams] = []
         self.blocks: t.List[Block] = []
-        self.cached_outputs: t.List[t.Any] = []
+        self.cached_outputs: t.Dict[str, t.Any] = {}
         objects = json.loads(serialized_pipeline)
         for cur_object in objects:
             cur_params = BlockParams(name=cur_object.name,
@@ -19,17 +19,17 @@ class Pipeline:
             cur_block = block_class(**cur_params.realization_params)
             self.block_params.append(block_class)
             self.blocks.append(cur_block)
-            self.cached_outputs.append(None)
+            self.cached_outputs[cur_object.name] = None
 
     def serialize(self) -> str:
         # TODO: add serialization
         pass
 
-    def run_pipeline(self, inputs: t.Any) -> t.Any:
+    def run_pipeline(self) -> t.Any:
         for block_idx, (cur_block, cur_params) in enumerate(zip(self.blocks, self.block_params)):
             needed_inputs = [self.cached_outputs[cur_input] for cur_input in cur_params.inputs]
             if None in needed_inputs:
                 raise RuntimeError(f'Some inputs for block {cur_params.name} on index {block_idx} '
                                    f'are calculated in the future')
-            self.cached_outputs[block_idx] = cur_block.run(needed_inputs)
-        return self.cached_outputs[-1]
+            self.cached_outputs[cur_params.name] = cur_block.run(needed_inputs)
+        return self.cached_outputs[self.block_params[-1].name]
--- src/craft_ml/utils/common.py
+++ src/craft_ml/utils/common.py
@@ -1,4 +1,5 @@
 import numpy as np
+from ..mltypes import KwArgs
 import typing as t
 
 
@@ -6,3 +7,7 @@ def ensure_numpy(array_like_object) -> np.ndarray:
     if isinstance(array_like_object, np.ndarray):
         return array_like_object
     return np.asarray(array_like_object)
+
+
+def initialize_class(class_name: str, arguments: KwArgs) -> object:
+    raise NotImplementedError
