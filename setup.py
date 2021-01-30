974710530d4f6e213dda93deb39c69dc5d683b3f
NV-27 <nvarganov77@gmail.com>
2021-01-31 01:51:43 +0300

    [UPDATE] Updated requirements.txt - add xgboost==0.90

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name="craft_ml",
     description="",
-    version="0.2.3",
+    version="0.2.4",
     package_dir={"": "src"},
     zip_safe=True,
     packages=find_packages('src'),
@@ -15,7 +15,8 @@ setup(
         "matplotlib==3.1.3",
         "pandas==1.0.1",
         "streamlit==0.75.0",
-        "dataclasses==0.6"
+        "dataclasses==0.6",
+        "xgboost==0.90"
     ],
     python_requires='>=3.7',
     setup_requires=[],
