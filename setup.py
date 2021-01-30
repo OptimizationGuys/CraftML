b3856e8446ce5a612b467e019747911e55f500d4
vuvko <notvuvko@gmail.com>
2021-01-30 23:47:07 +0300

    Python interpreter version.

--- setup.py
+++ setup.py
@@ -14,11 +14,11 @@ setup(
         "numpy==1.18.1",
         "scikit_learn==0.24.1",
         "matplotlib==3.1.3",
-        "dataclasses==0.8",
         "streamlit",
         "pandas==1.0.1",
         "streamlit==0.75.0",
     ],
+    python_requires='>=3.7',
     setup_requires=[],
     entry_points={
         "console_scripts": [
