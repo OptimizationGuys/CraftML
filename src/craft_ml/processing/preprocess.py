25a4428d4925900581a186ca900caa4fb5a064c5
vuvko <notvuvko@gmail.com>
2021-01-30 12:36:56 +0300

    Fixed differences inside the dataset. Added transforms.

--- src/craft_ml/processing/preprocess.py
+++ src/craft_ml/processing/preprocess.py
@@ -7,10 +7,11 @@ import typing as t
 
 def findna(dataset: TableDataset) -> TableDataset:
     data = dataset.table_data
-    data[data == 'nan'] = None
-    data[data == 'NaN'] = None
-    data[data == 'NA'] = None
-    data[data == 'na'] = None
+    # data[data == 'nan'] = None
+    # data[data == 'NaN'] = None
+    # data[data == 'NA'] = None
+    # data[data == 'na'] = None
+    data.replace(('nan', 'NaN', 'NA', 'na', np.nan), None, inplace=True)
     dataset.table_data = data
     return dataset
 
@@ -18,34 +19,12 @@ def findna(dataset: TableDataset) -> TableDataset:
 def fillna(dataset: TableDataset) -> TableDataset:
     if isinstance(dataset.table_data, pd.DataFrame):
         dataset.table_data.fillna(method='ffill', inplace=True)
+        dataset.table_data.fillna(method='bfill', inplace=True)
     else:
         mask = np.isnan(dataset.table_data)
         idx = np.where(~mask, np.arange(mask.shape[1]), 0)
         np.maximum.accumulate(idx, axis=1, out=idx)
         dataset.table_data = dataset.table_data[np.arange(idx.shape[0])[:, None], idx]
-    return dataset
-
-
-def to_category(dataset: TableDataset, max_unique: int = 20) -> TableDataset:
-    def column_iterator(dataset: TableDataset) -> t.Generator[t.Union[np.ndarray, pd.Series]]:
-        data = dataset.table_data
-        if isinstance(data, pd.DataFrame):
-            return (data[col_name] for col_name in data.columns)
-        else:
-            return (data[:, col_idx] for col_idx in range(data.shape[1]))
-
-    def get_unique(column: t.Union[np.ndarray, pd.Series]):
-        if isinstance(column, pd.Series):
-            return column.unique()
-        else:
-            return np.unique(column)
-
-    for column in column_iterator(dataset):
-        unique = get_unique(column)
-        if len(unique) > max_unique:
-            continue
-        for cur_idx, cur_value in enumerate(unique):
-            column[column == cur_value] = cur_idx
     return dataset
 
 
