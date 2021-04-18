37b52499de397650419fc7a271a8e1c307bf9931
vuvko <notvuvko@gmail.com>
2021-04-18 19:31:17 +0300

    AppImage

--- setup.py
+++ setup.py
@@ -10,15 +10,15 @@ setup(
     zip_safe=True,
     packages=find_packages('src'),
     install_requires=[
-        "numpy==1.18.1",
-        "scikit_learn==0.24.1",
-        "matplotlib==3.1.3",
-        "pandas==1.0.1",
-        "streamlit==0.75.0",
-        "dataclasses==0.6",
-        "xgboost==0.90"
+        "numpy>=1.19.1",
+        "scikit_learn>=0.24.1",
+        "matplotlib>=3.1.3",
+        "pandas>=1.0.1",
+        "streamlit>=0.75.0",
+        # "dataclasses>=0.6",
+        "xgboost>=0.90"
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.6',
     setup_requires=[],
     entry_points={
         "console_scripts": [
